16b6c8d7633b350747dea9f6f5b91cec8fa64276
paperjuice <paperjuiceinc@gmail.com>
2026-06-18 14:03:02 +0300

    [SCH1] Init project

---
+++ lib/school_web/router.ex
@@ -0,0 +1,27 @@
+defmodule SchoolWeb.Router do
+  use SchoolWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {SchoolWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", SchoolWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SchoolWeb do
+  #   pipe_through :api
+  # end
+end
