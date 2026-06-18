e80ebce3c2f1324255c5b500a9897a5ae3e8cd93
paperjuice <paperjuiceinc@gmail.com>
2026-06-18 14:58:07 +0300

    [SCH4] Add validation function for Package in Logic

--- lib/school/logic.ex
+++ lib/school/logic.ex
@@ -23,7 +23,29 @@ defmodule School.Logic do
     }
   end
 
-  def validate_rule1(%{type: :letter, weight: weight}) do
+  def validate(package, rules_to_apply) do
+    [
+      rule1: &validate_rule1/1,
+      rule2: &validate_rule2/1,
+      rule3: &validate_rule3/1,
+      rule4: &validate_rule4/1,
+      rule5: &validate_rule5/1,
+      rule6: &validate_rule6/1,
+      rule7: &validate_rule7/1,
+      rule8: &validate_rule8/1,
+      rule9: &validate_rule9/1,
+      rule10: &validate_rule10/1
+    ]
+    |> Enum.filter(fn {rule, _} -> rule in rules_to_apply end)
+    |> Enum.reduce_while({:valid, "success"}, fn {_rule, func}, acc ->
+      case func.(package) do
+        {:valid, _} -> {:cont, acc}
+        {:invalid, msg} -> {:halt, {:invalid, msg}}
+      end
+    end)
+  end
+
+  defp validate_rule1(%{type: :letter, weight: weight}) do
     if weight < 500 do
       {:valid, "rule1"}
     else
@@ -31,22 +53,22 @@ defmodule School.Logic do
     end
   end
 
-  def validate_rule1(_), do: {:valid, "rule1"}
+  defp validate_rule1(_), do: {:valid, "rule1"}
 
-  def validate_rule2(%{destination: :international, has_customs_form: true}),
+  defp validate_rule2(%{destination: :international, has_customs_form: true}),
     do: {:valid, "rule2"}
 
-  def validate_rule2(%{destination: :international, has_customs_form: false}),
+  defp validate_rule2(%{destination: :international, has_customs_form: false}),
     do: {:invalid, "Internation requires customs form"}
 
-  def validate_rule2(_), do: {:valid, "rule2"}
+  defp validate_rule2(_), do: {:valid, "rule2"}
 
-  def validate_rule3(%{type: :fragile, shipping_class: :standard}),
+  defp validate_rule3(%{type: :fragile, shipping_class: :standard}),
     do: {:invalid, "Fragile can't use standard shipping"}
 
-  def validate_rule3(_), do: {:valid, "rule3"}
+  defp validate_rule3(_), do: {:valid, "rule3"}
 
-  def validate_rule4(%{type: :parcel, weight: weight, shipping_class: :priority}) do
+  defp validate_rule4(%{type: :parcel, weight: weight, shipping_class: :priority}) do
     if weight < 5000 do
       {:valid, "rule4"}
     else
@@ -54,10 +76,10 @@ defmodule School.Logic do
     end
   end
 
-  def validate_rule4(_), do: {:valid, "rule4"}
+  defp validate_rule4(_), do: {:valid, "rule4"}
 
-  def validate_rule5(%{declared_value: declared_value, has_insurance: has_insurance})
-      when declared_value > 100 do
+  defp validate_rule5(%{declared_value: declared_value, has_insurance: has_insurance})
+       when declared_value > 100 do
     if has_insurance do
       {:valid, "rule5"}
     else
@@ -65,35 +87,35 @@ defmodule School.Logic do
     end
   end
 
-  def validate_rule5(_), do: {:valid, "rule5"}
+  defp validate_rule5(_), do: {:valid, "rule5"}
 
-  def validate_rule6(%{type: :fragile, has_fragile_sticker: true}), do: {:valid, "rule6"}
+  defp validate_rule6(%{type: :fragile, has_fragile_sticker: true}), do: {:valid, "rule6"}
 
-  def validate_rule6(%{type: :fragile, has_fragile_sticker: false}),
+  defp validate_rule6(%{type: :fragile, has_fragile_sticker: false}),
     do: {:invalid, "missing fragile sticker for fragile package"}
 
-  def validate_rule6(_), do: {:valid, "rule6"}
+  defp validate_rule6(_), do: {:valid, "rule6"}
 
-  def validate_rule7(%{destination: :eu, shpping_class: shipping_class})
-      when shipping_class in [:express, :priority],
-      do: {:valid, "rule7"}
+  defp validate_rule7(%{destination: :eu, shpping_class: shipping_class})
+       when shipping_class in [:express, :priority],
+       do: {:valid, "rule7"}
 
-  def validate_rule7(%{destination: :international, shipping_class: shipping_class})
-      when shipping_class in [:express, :priority],
-      do: {:valid, "rule7"}
+  defp validate_rule7(%{destination: :international, shipping_class: shipping_class})
+       when shipping_class in [:express, :priority],
+       do: {:valid, "rule7"}
 
-  def validate_rule7(%{destination: destination, shipping_class: shipping_class})
-      when destination in [:eu, :international],
-      do: {:invalid, "#{destination} has wrong shipping class: #{shipping_class}"}
+  defp validate_rule7(%{destination: destination, shipping_class: shipping_class})
+       when destination in [:eu, :international],
+       do: {:invalid, "#{destination} has wrong shipping class: #{shipping_class}"}
 
-  def validate_rule7(_package), do: {:valid, "rule7"}
+  defp validate_rule7(_package), do: {:valid, "rule7"}
 
-  def validate_rule8(%{type: :letter, has_insurance: true}),
+  defp validate_rule8(%{type: :letter, has_insurance: true}),
     do: {:invalid, "letters can't have insurance"}
 
-  def validate_rule8(_package), do: {:valid, "rule8"}
+  defp validate_rule8(_package), do: {:valid, "rule8"}
 
-  def validate_rule9(%{shipping_class: :standard, destination: :domestic, weight: weight}) do
+  defp validate_rule9(%{shipping_class: :standard, destination: :domestic, weight: weight}) do
     if weight < 2000 do
       {:valid, "rule9"}
     else
@@ -101,35 +123,35 @@ defmodule School.Logic do
     end
   end
 
-  def validate_rule10(%{
-        type: :fragile,
-        destination: :international,
-        shipping_class: shipping_class,
-        weight: weight
-      })
-      when weight > 1000 do
+  defp validate_rule10(%{
+         type: :fragile,
+         destination: :international,
+         shipping_class: shipping_class,
+         weight: weight
+       })
+       when weight > 1000 do
     if shipping_class == :priority,
       do: {:valid, "rule10"},
       else: {:invalid, "fragile internationle packages over 1000g must use priority"}
   end
 
-  def validate_rule10(_package) do
+  defp validate_rule10(_package) do
     {:valid, "rule10"}
   end
 
-  def calculate_weight(:letter), do: Enum.random(1..600)
-  def calculate_weight(_), do: Enum.random(1..10000)
+  defp calculate_weight(:letter), do: Enum.random(1..600)
+  defp calculate_weight(_), do: Enum.random(1..10000)
 
-  def value_range do
+  defp value_range do
     10..4000
     |> Range.to_list()
     |> build_value_range([])
     |> Enum.reverse()
   end
 
-  def build_value_range([], acc), do: acc
+  defp build_value_range([], acc), do: acc
 
-  def build_value_range([hd | tl], acc) do
+  defp build_value_range([hd | tl], acc) do
     to_float = hd / 10
     new_acc = [to_float | acc]
 
