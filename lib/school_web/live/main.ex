7612a7d7b7507fba5511bd2477d2ec24845c3312
paperjuice <paperjuiceinc@gmail.com>
2026-06-18 15:38:20 +0300

    [SCH5] Add swipe behaviour

--- lib/school_web/live/main.ex
+++ lib/school_web/live/main.ex
@@ -1,20 +1,57 @@
 defmodule SchoolWeb.MainLive do
   use SchoolWeb, :live_view
 
-  alias School.Package
+  alias School.Logic
 
   import SchoolWeb.GameComponents
 
   @impl true
   def mount(_params, _session, socket) do
+    package = Logic.generate_package()
+
     new_socket =
       socket
       |> assign(:local_player, true)
-      |> assign(:package, %Package{})
+      |> assign(:package, package)
       |> assign(:timestamp, nil)
       |> assign(:validation_result, nil)
       |> assign(:game_state, :in_progress)
 
     {:ok, new_socket}
+  end
+
+  @impl true
+  def handle_event("approve", _params, socket) do
+    new_socket = validation("swipe-right", socket)
+
+    {:noreply, new_socket}
+  end
+
+  @impl true
+  def handle_event("decline", _params, socket) do
+    new_socket = validation("swipe-left", socket)
+
+    {:noreply, new_socket}
+  end
+
+  @impl true
+  def handle_info(:next_package, socket) do
+    package = Logic.generate_package()
+
+    new_socket =
+      socket
+      |> assign(:package, package)
+      |> push_event("reset-package-card", %{})
+
+    {:noreply, new_socket}
+  end
+
+  defp validation(swipe_direction, socket) do
+    new_socket =
+      push_event(socket, swipe_direction, %{})
+
+    Process.send_after(self(), :next_package, 1_000)
+
+    new_socket
   end
 end
